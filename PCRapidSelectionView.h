423679ce90c435d34cf065c48cf356e33ac27fbf
Maximilian Mackh <mmackh@gmail.com>
2017-04-11 08:12:06 +0200

    Update PCRapidSelectionView.h

--- PCRapidSelectionView.h
+++ PCRapidSelectionView.h
@@ -10,8 +10,21 @@
 
 extern NSInteger const kPCRapidSelectionViewTag;
 
+typedef enum : NSUInteger
+{
+    IPDFUIThemeLight,
+    IPDFUIThemeDark,
+} IPDFUITheme;
+
+
 @interface PCRapidSelectionView : UIView
 
 + (void)viewForParentView:(UIView *)parentView currentGuestureRecognizer:(UIGestureRecognizer *)guestureRecognizer interactive:(BOOL)interactive options:(NSArray *)options title:(NSString *)title completionHandler:(void(^)(NSInteger selectedIndex))completionHandler;
+
++ (void)viewForParentView:(UIView *)parentView theme:(IPDFUITheme)theme currentGuestureRecognizer:(UIGestureRecognizer *)guestureRecognizer interactive:(BOOL)interactive options:(NSArray *)options title:(NSString *)title completionHandler:(void(^)(NSInteger selectedIndex))completionHandler;
+
+@end
+
+@interface PCRapidButton : UIButton
 
 @end
